c28e54d5debd434b9b4952731836caf051d51ee6
zzf7ktx <41227962+zzf7ktx@users.noreply.github.com>
2022-02-15 01:37:24 +0700

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,21 @@
+require("@nomiclabs/hardhat-waffle");
+const fs = require("fs");
+const privateKey = fs.readFileSync(".secret").toString()
+const projectId = 'b6b79e45c3f541de8f0e5fb152a7a2a8';
+
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    }
+  },
+  solidity: "0.8.4",
+};

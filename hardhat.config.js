0a5beea1a21d1ed6a6bb8f87cd59c8b8c7e894af
zzf7ktx <zzf7ktx@gmail.com>
2022-02-25 17:21:57 +0700

    Add dockerfile

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,25 @@
 require("@nomiclabs/hardhat-waffle");
 const fs = require("fs");
-const privateKey = fs.readFileSync(".secret").toString()
-const projectId = 'b6b79e45c3f541de8f0e5fb152a7a2a8';
+const privateKey = fs.readFileSync(".secret").toString();
+const projectId = "b6b79e45c3f541de8f0e5fb152a7a2a8";
 
 module.exports = {
   networks: {
     hardhat: {
-      chainId: 1337
+      chainId: 1337,
     },
     mumbai: {
       url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
-      accounts: [privateKey]
+      accounts: [privateKey],
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${projectId}`,
+      accounts: [privateKey],
     },
     mainnet: {
       url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
-      accounts: [privateKey]
-    }
+      accounts: [privateKey],
+    },
   },
   solidity: "0.8.4",
 };
